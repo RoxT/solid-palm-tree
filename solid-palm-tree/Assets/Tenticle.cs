143daab41536031bcc4ec5a7c5f486ba95594eaa
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2019-01-26 11:33:51 -0800

    Tenticle prefab

---
+++ solid-palm-tree/Assets/Tenticle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tenticle : MonoBehaviour {
+
+    private HingeJoint joint;
+    private JointMotor motor;
+    private int collisionDebounce = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        joint = GetComponent<HingeJoint>();
+        motor = joint.motor;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (collisionDebounce > 0)
+        {
+            collisionDebounce--;
+        }
+	}
+
+    private void OnCollision(Collision collision)
+    {
+            JointMotor motor = joint.motor;
+            Debug.Log("1. targetVelocity : " + motor.targetVelocity);
+            motor.targetVelocity = -joint.motor.targetVelocity;
+            joint.motor = motor;
+            collisionDebounce = 10;
+            Debug.Log("2. targetVelocity : " + motor.targetVelocity);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collisionDebounce <= 0)
+        {
+            JointMotor motor = joint.motor;
+            Debug.Log("1. targetVelocity : " + motor.targetVelocity);
+            motor.targetVelocity = -joint.motor.targetVelocity;
+            joint.motor = motor;
+            collisionDebounce = 10;
+            Debug.Log("2. targetVelocity : " + motor.targetVelocity);
+        }
+
+    }
+}
