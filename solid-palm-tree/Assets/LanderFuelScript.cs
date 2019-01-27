fc78ad31402b0ae3e63379fefc9ffdc1741b5ff6
Chris Dileo <christopher.dileo@sap.com>
2019-01-27 09:46:33 -0800

    Fixed lander prefab error

---
+++ solid-palm-tree/Assets/LanderFuelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanderFuelScript : MonoBehaviour {
+
+    [SerializeField]
+    private float fuelLevel;
+    [SerializeField]
+    private GameObject lander;
+    private LanderMovementScript lms;
+    [SerializeField]
+    private Image fillImage;
+
+	// Use this for initialization
+	void Start () {
+        fillImage = GetComponent<Image>();
+        lms = lander.GetComponentInChildren<LanderMovementScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fuelLevel = lms.GetFuelLevel();
+        fillImage.fillAmount = fuelLevel;
+    }
+}
