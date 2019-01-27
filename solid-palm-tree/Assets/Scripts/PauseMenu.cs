d0dc67bee397d27b4487418a70ce0b51f1ca1a6d
Charlotte Schofield <cschofield@shaw.ca>
2019-01-26 22:32:07 -0800

    Changes To Scripts And Scenes
    Changed the Scripts and Scenes to the proper folders.

---
+++ solid-palm-tree/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Created By: Charlotte Schofield
+//AKA: Aquillis
+
+//Note: Make sure everything else works properly!
+
+public class PauseMenu : MonoBehaviour
+{
+    //Checks to see if game is paused
+    public static bool GameIsPaused = false;
+
+    //Controls the UI for Pausing
+    public GameObject pausingMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Checks the bool to see if game is paused
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    //Method for resuming the game
+    public void Resume ()
+    {
+        //Deactivate the UI for pausing
+        pausingMenuUI.SetActive(false);
+        //For Resuming time
+        Time.timeScale = 1f;
+        //changes bool to false
+        GameIsPaused = false;
+    }
+    //method for pausing
+    private void Pause ()
+    {
+        //activate the UI for pausing
+        pausingMenuUI.SetActive(true);
+        //For Freezing time
+        Time.timeScale = 0f;
+        //changes bool to true
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //Make sure that this isn't deleted, otherwise it's softlocked!
+        Time.timeScale = 1f;
+        //Note: Get this removed & given it's own var later
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void CloseGame()
+    {
+        //As it's still being built, it will do nothing for now.
+        Debug.Log("Game Closed...");
+        Application.Quit();
+    }
+}
---
+++ solid-palm-tree/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+//Created by: Charlotte Schofield
+//AKA: Aquillis
+//Note: Please add script to actual Player Script
+//This code will NOT work otherwise!!
+
+
+public class Player : MonoBehaviour
+{
+    //Activates the Stats class
+    //and allows changes to occur in real-time
+    [SerializeField]
+    private Stats fuel;
+
+    //Removed the common Start() as we need to have the vals in Fuel awake.
+    private void Awake()
+    {
+        //Activate the bar with initilize
+        fuel.Initilize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Activates when A key is pressed. Best use for the meter.
+        //Note to self: do NOT change from this function!
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            fuel.CurrentVal -= 10f;
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            fuel.CurrentVal += 10f;
+        }
+    }
+}
